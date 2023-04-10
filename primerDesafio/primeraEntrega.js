4957090cad076197325c9c6a1049a8fd9f7cb828
Alan Pozzi <alanpozzi@gmail.com>
2023-04-10 15:11:10 -0300

    segunda entrega

--- primerDesafio/primeraEntrega.js
+++ primerDesafio/primeraEntrega.js
@@ -9,27 +9,28 @@ class ProductManager{
         price, 
         thumbnail, 
         code, 
-        stock
+        stock,
+       
     }
 
 
 //validaciones de codigo
 
-const validateCode = this.products.find(produc=> produc.code === product.code)
-if (validateCode){
-    return console.log("producto con codigo repetido")
-}
+    const validateCode = this.products.find(produc=> produc.code === product.code)
+    if (validateCode){
+            return console.log("producto con codigo repetido")
+        }
 
-if(!Object.values(product).every(prod=>prod)){
-    return console,log("producto incompleto")
-}
+    if(!Object.values(product).every(prod=>prod)){
+            return console,log("producto incompleto")
+        }
 
 
-if(this.products.lenght === 0){
-    product.id = 1
-} else{
-    product.id = this.products.lenght + 1
-}
+    if(this.products.length === 0){
+        product.id = 1
+        } else{
+        product.id = this.products.length + 1
+    }
 
 
     this.products.push(product)
@@ -44,7 +45,7 @@ getproduct(){
 };
 
 getproductsById (id){
-    const validateid = this.products.find(produc => produc.id === id)
+    const validateid = this.products.findIndex((produc) => produc.id === id)
     if(validateid){
         return console.log(this.products[id-1])
     } else{
@@ -58,10 +59,10 @@ getproductsById (id){
 const produc = new ProductManager()
 
 produc.addProducts("mesa", "mesa rectangular", 100, "sin imagen", 1, 50 )
-produc.addProducts("silla", "con almohadon", 70, "sin imagen", 50, 20)
+produc.addProducts("silla", "con almohadon", 70, "sin imagen", 1, 20)
 produc.addProducts("mesa", "mesa ratona", 80, "sin imagen", 13, 15 )
 produc.addProducts("lampara", "de pie", 50, "sin imagen", 60, 50)
 
 produc.getproduct()
 
-produc.getproductsById(4)
+
